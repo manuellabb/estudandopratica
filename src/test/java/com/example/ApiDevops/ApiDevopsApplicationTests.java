a6f5514ccdd540083ee02c80f8a22ef1fa8fe553
Manuella Bernardinelli <manuellabernardinelliborges@gmail.com>
2023-09-27 09:29:08 -0300

    commitando

---
+++ src/test/java/com/example/ApiDevops/ApiDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiDevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
